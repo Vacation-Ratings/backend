9816c270a58304329c6cee4b172a0db059be2310
Keian Anthony <KeianAnthony1@gmail.com>
2022-03-16 14:03:32 -0700

    adds model change to allow a string of any length for the country

--- src/models/vacation.js
+++ src/models/vacation.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const vacationSchema = new mongoose.Schema({
   // City
   location: { type: String, required: true },
-  country: { type: String, required: true, maxlength: 4 },
+  country: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number },
   rating: { type: Number, required: true, min: 0, max: 5 },
